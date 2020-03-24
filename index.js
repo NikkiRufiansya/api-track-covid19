d8a627ec1ad5e897392fee7fad3f673da1ecf55f
NikkiRufiansya <nikkirufiansya88@gmail.com>
2020-03-24 20:54:51 +0700

    make it better

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-// const express = require('express')
-// const app = express()
-// const port = 8000
-// const covid = require('novelcovid');
-
- 
-// // Now we create a async/await
-// (async () => {
- 
-//     let specificCountry = await covid.getCountry({country: 'Indonesia'});
-   
-//     return app.get('/', (req, res) => res.json(specificCountry))
-// })()
-
-
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 4000
-app.get('/', (req, res) => res.send('<h2> Hello World! </h2>'));
-app.listen(PORT, () => console.log(`app listening on port ${PORT}`))
+const covid = require('novelcovid');
+
+ 
+// Now we create a async/await
+(async () => {
+ 
+    let specificCountry = await covid.getCountry({country: 'Indonesia'});
+   
+    return app.get('/', (req, res) => res.json(specificCountry))
+})()
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+// app.get('/', (req, res) => res.send('<h2> Hello World! </h2>'));
+// app.listen(PORT, () => console.log(`app listening on port ${PORT}`))
