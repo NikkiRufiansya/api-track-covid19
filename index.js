fe82e6c08f15af60d39e7db40ef27c1e8cee6d7d
NikkiRufiansya <nikkirufiansya88@gmail.com>
2020-03-24 22:28:57 +0700

    push baru

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 2000
 const covid = require('novelcovid');
 
+
  
-// Now we create a async/await
-(async () => {
+app.get('/', function (req, res) {
+    let doc = "Dokumentasi Resy API : <br>\
+                All Country : appku19.herokuapp.com/all <br>\
+                Country Indonesia : appku19.herokuapp.com/indonesia <br>\
+                by : Backend(Nikky Rufiansya), Frontend(VerryAnto)"
+    return res.send(doc);
+})
+
+app.get('/indonesia', function (req, res) {
+    (async () => {
  
-    let specificCountry = await covid.getCountry({country: 'Indonesia'});
-   
-    return app.get('/', (req, res) => res.json(specificCountry))
-})()
+        let specificCountry = await covid.getCountry({country: 'Indonesia'});
+       
+        return res.json(specificCountry)
+    })()
+})
+
+app.get('/all', function (req, res) {
+    (async () => {
+        let sortedCountries = await covid.getCountry({sort: 'recovered'});
+        return res.json(sortedCountries);
+    })()
+})
+
 
 
 app.listen(PORT, () => console.log(`app listening on port ${PORT}`))
