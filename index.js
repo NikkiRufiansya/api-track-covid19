b87c70133d779f999c4d60c84dcae9e07ed9d128
NikkiRufiansya <nikkirufiansya88@gmail.com>
2020-03-26 13:32:25 +0700

    push baru

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 2000
 const covid = require('novelcovid');
-
-
  
 app.get('/', function (req, res) {
     let doc = " Dokumentasi Rest API : <br><br>\
                 All Country : appku19.herokuapp.com/all <br><br>\
                 Country Indonesia : appku19.herokuapp.com/indonesia <br><br>\
                 Country Parameter : appku19.herokuapp.com/countries/pilih_negara_terserah_lu <br><br>\
-                Country Parameter : appku19.herokuapp.com/getAll <br><br>\
+                All Cases, Recovered, & Deaths in the world : appku19.herokuapp.com/getAll <br><br>\
                 Website : on progress<br><br>\
                 by : Backend(Nikky), Frontend(Verry)"
     return res.send(doc);
