681b947cc1648675eeb08647cb370bc2b9ea19e2
NikkiRufiansya <nikkirufiansya88@gmail.com>
2020-03-26 14:48:55 +0700

    update all cases in the world, historical, & province

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ app.get('/', function (req, res) {
     let doc = " Dokumentasi Rest API : <br><br>\
                 All Country : appku19.herokuapp.com/all <br><br>\
                 Country Indonesia : appku19.herokuapp.com/indonesia <br><br>\
-                Country Parameter : appku19.herokuapp.com/countries/pilih_negara_terserah_lu <br><br>\
+                Country Parameter : appku19.herokuapp.com/countries/{country} <br><br>\
                 All Cases, Recovered, & Deaths in the world : appku19.herokuapp.com/getAll <br><br>\
+                Historical Parameter : appku19.herokuapp.com/getHistorical/{country}' <br><br>\
+                Province Parameter : appku19.herokuapp.com//getProvince/{params}'\
+                params is : (semua, aceh, banten, jatim, jateng, jabar, lampung, ntb)\
                 Website : on progress<br><br>\
                 by : Backend(Nikky), Frontend(Verry)"
     return res.send(doc);
@@ -48,5 +51,19 @@ app.get('/getAll', function (req, res) {
     })()
 })
 
+app.get('/getHistorical/:country', function (req, res) {
+    (async () => {
+ 
+        let historical = await covid.getHistorical({country: req.params.country});  
+        return res.json(historical)
+    })()
+})
 
+app.get('/getProvince/:params', function (req, res) {
+    (async () => {
+ 
+        let historical = await covid.getProv({country: req.params.params});  
+        return res.json(historical)
+    })()
+})
 app.listen(PORT, () => console.log(`app listening on port ${PORT}`))
